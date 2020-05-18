1a08c373a7d1cf06e0f449dcd0ff1020966f96da
Theo Massard <theo@memento-solution.com>
2020-05-18 10:11:42 +0200

    Add correct bonus querying script

---
+++ search.py
@@ -0,0 +1,34 @@
+"""Query program for trail."""
+import sys
+import os
+
+import pymongo
+
+
+def main():
+    uri = os.environ.get("MONGODB_URI")
+    database = pymongo.MongoClient(uri).get_default_database()
+
+    try:
+        query = sys.argv[1]
+    except IndexError:
+        print("Usage: ./search.py term", file=sys.stderr)
+        sys.exit(1)
+
+    docs = database.logs.find({
+        "message": {
+            "$regex": f".*{query}.*",
+        },
+    }, {
+        '@timestamp': 1,
+        'message': 1,
+        '_id': 0,
+    }, ).sort('@timestamp')
+    print('@timestamp,message')
+    for doc in docs:
+        print(doc['@timestamp'], doc['message'], sep=',')
+        return
+
+
+if __name__ == '__main__':
+    main()
